0c23bca76198929f743fcf8f20d9a05d96bd5cbd
witt <mymeek@163.com>
2020-10-28 16:51:59 +0800

    服务端返回 body 长度

--- Week_02/src/main/java/org/geekbang/service/ServerService.java
+++ Week_02/src/main/java/org/geekbang/service/ServerService.java
@@ -10,24 +10,26 @@ public class ServerService {
 
     public static void service(Socket socket) {
         try {
-            // request
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            StringBuilder params = new StringBuilder();
-            String s;
-            while ((s = reader.readLine()) != null) {
-                params.append(s).append("\r\n");
-                if (s.length() <= 0) {
-                    break;
-                }
-            }
-            System.out.println(params.toString());
+            //request
+            //BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //StringBuilder params = new StringBuilder();
+            //String s;
+            //while ((s = reader.readLine()) != null) {
+            //    params.append(s).append("\r\n");
+            //    if (s.length() <= 0) {
+            //        break;
+            //    }
+            //}
+            //System.out.println(params.toString());
 
-            // response
+            //response
             PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
             printWriter.println("HTTP/1.1 200 OK");
             printWriter.println("Content-Type: text/html;charset=UTF-8");
-            printWriter.println("\r");
-            printWriter.println("<html>Hello NIO</html>");
+            String body = "<html>Hello NIO</html>";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.println(body);
             //socket.shutdownOutput();
             printWriter.close();
             socket.close();
