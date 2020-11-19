8c6d4de8825a2f0fca3d94ad7ae6c1be3181c574
witt <mymeek@163.com>
2020-11-20 01:19:24 +0800

    初始化 Week_06 项目

---
+++ Week_06/homework-google-guava/src/main/java/org/geekbang/GuavaExample.java
@@ -0,0 +1,5 @@
+package org.geekbang;
+
+public class GuavaExample {
+
+}
---
+++ Week_06/homework-google-guava/src/test/java/org/geekbang/GuavaExampleTest.java
@@ -0,0 +1,10 @@
+package org.geekbang;
+
+import org.junit.Test;
+
+public class GuavaExampleTest {
+
+    @Test
+    public void funTest() {
+    }
+}
